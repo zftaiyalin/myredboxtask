c553f72553f8afdaf6baf05ad74774f2cde96524
曾富田 <zftaife@163.com>
2017-08-23 09:55:15 +0800

    Initial Commit

---
+++ MyRedTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRedTask
+//
+//  Created by 曾富田 on 2017/8/23.
+//  Copyright © 2017年 曾富田. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
